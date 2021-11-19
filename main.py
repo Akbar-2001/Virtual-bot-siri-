5f5db34d1783dd69091bdd2dce7394d551cc5168
Akbar pasha <91407391+Akbar-2001@users.noreply.github.com>
2021-11-19 14:17:52 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'chitti' in command:
+                command = command.replace('chitti', ' ')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_chitti():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print('current time is ' + time)
+        talk('current time is ' + time)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'what is' in command:
+        person = command.replace('what is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'open whatsapp' in command:
+        codePath = "C:\\Users\\AKBAR PASHA\\OneDrive\\Desktop\\WhatsApp Desktop.lnk"
+        os.startfile(codePath)
+
+    else:
+        talk('please say the command again.')
+
+
+while True:
+    run_chitti()
